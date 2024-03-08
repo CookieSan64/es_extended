b46a786d857452b3faf99e4f56f28225f8db378d
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 01:29:24 +0100

    Initial commit

---
+++ config.logs.lua
@@ -0,0 +1,23 @@
+Config.DiscordLogs = {
+    Webhooks = {
+        default = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+        test = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+        Chat = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+        UserActions = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+        Resources = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+        Paycheck = "https://discord.com/api/webhooks/999998308791627786/r6Ju9-7Uei278L58pCE8Z2s11qMXAzHEEO-plz1ZXMbraaPNCQsu8hjNiSAG4daVpmoR",
+    },
+
+    Colors = { -- https://www.spycolor.com/
+        default = 14423100,
+        blue = 255,
+        red = 16711680,
+        green = 65280,
+        white = 16777215,
+        black = 0,
+        orange = 16744192,
+        yellow = 16776960,
+        pink = 16761035,
+        lightgreen = 65309,
+    },
+}
